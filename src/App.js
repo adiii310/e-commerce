fe339e3ad96a554383f905f0c4f70d95f088daff
adii <adityathakur6354@gmail.com>
2024-01-09 11:39:04 +0530

    clearing app component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 import Navbar from "./Components/Navbar";
@@ -12,28 +12,9 @@ import Women from "./Catagories/Women";
 import New from "./Catagories/New";
 
 import './App.css';
-import { Menswear } from "./Catagories/Menswear";
 
 function App() {
-  const [mensWearData,setMensWearData] = useState(Menswear)
 
-  const LOCAL_STORAGE_KEY = 'fav'
-  const handleFav = (id, e) => {
-    e.preventDefault();
-    const updatedData = [...mensWearData];
-    const itemIndex = updatedData.findIndex(item => item.id === id);
-
-    if (itemIndex !== -1) {
-      updatedData[itemIndex] = {
-        ...updatedData[itemIndex],
-        favorite: !updatedData[itemIndex].favorite,
-      };
-      const fav = updatedData.filter(item => item.favorite)
-      localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(fav))
-      setMensWearData(updatedData);
-
-    }
-  };
   return (
     <>
     <BrowserRouter>
@@ -41,8 +22,8 @@ function App() {
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/product' element={<Product/>} />
-        <Route path="/men/:id" element={<ProductDetails handleFav={(id,e)=>handleFav(id,e)}/>} />
-        <Route path='/men' element={<Men handleFav={(id,e)=>handleFav(id,e)}/>} />
+        <Route path="/men/:id" element={<ProductDetails />} />
+        <Route path='/men' element={<Men />} />
         <Route path='/women' element={<Women/>} />
         <Route path='/new' element={<New/>} />
       </Routes>
